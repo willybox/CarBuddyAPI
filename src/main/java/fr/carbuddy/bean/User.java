49ccd8cb096a00bfc5f1bb5e03d5d21345efd047
WillyBox <ung.willybox@gmail.com>
2018-08-09 17:47:48 +0200

    Added user name attribute

--- src/main/java/fr/carbuddy/bean/User.java
+++ src/main/java/fr/carbuddy/bean/User.java
@@ -5,13 +5,22 @@ import fr.carbuddy.enumeration.string.StatusUser;
 public class User extends Person {
 	
 	private StatusUser statusUser;
+	private String userName;
 
 	public StatusUser getStatusUser() {
 		return statusUser;
 	}
 
+	public String getUserName() {
+		return userName;
+	}
+
 	public void setStatusUser(StatusUser statusUser) {
 		this.statusUser = statusUser;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 
 }
